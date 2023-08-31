fe7bdb134a61447e034f0eb4c98cd25b81cb99d7
lisnote <1292580280@qq.com>
2023-09-01 01:37:30 +0800

    feat: 完成基本逻辑

--- JavaScript/TypewriterAndEraser/index.js
+++ JavaScript/TypewriterAndEraser/index.js
@@ -2,12 +2,14 @@
  * * 流程
  * 1. 打印原文
  * 2. 操作文本
+ * 3. 清空文本
  *
  * * 功能函数
  * 1. 打字机
  * 2. 橡皮擦
  */
 import { diffWords } from "diff";
+
 // 功能函数 ------------------------------------------------------------
 // 打字机
 async function typewriter({
@@ -20,7 +22,7 @@ async function typewriter({
   const before = source.slice(0, index);
   const after = source.slice(index);
   const target = before + input[0] + after;
-  el.innerHTML = target;
+  el.innerHTML = before + input[0] + `<span class="cursor"></span>` + after;
   if (input.length > 1) {
     return new Promise((resolve) => {
       setTimeout(
@@ -51,7 +53,7 @@ async function eraser({
   const before = source.slice(0, index - 1);
   const after = source.slice(index);
   const target = before + after;
-  el.innerText = target;
+  el.innerHTML = before + `<span class="cursor"></span>` + after;
   if (length > 1) {
     return new Promise((resolve) => {
       setTimeout(
@@ -78,11 +80,14 @@ It allows you to create interactive and dynamic web pages.`;
 const targetText = `JavaScript is a widely-used programming language for building web applications.
 It enables the creation of responsive and interactive web interfaces.`;
 
-// 1. 打印原文
-await typewriter({ input: sourceText, delay: 10 });
-
-// 2. 操作文本
-async function textDiffStart() {
+async function textDiffChange({
+  delay = 100,
+  typewriterDelay,
+  eraserDelay,
+} = {}) {
+  // 1. 打印原文
+  await typewriter({ input: sourceText, delay: typewriterDelay || delay });
+  // 2. 操作文本
   const diff = diffWords(sourceText, targetText);
   let indexCount = 0;
   let workingText = sourceText;
@@ -92,7 +97,7 @@ async function textDiffStart() {
         input: task.value,
         index: indexCount,
         source: workingText,
-        delay: 50,
+        delay,
       });
       indexCount += task.value.length;
     } else if (task.removed) {
@@ -100,13 +105,19 @@ async function textDiffStart() {
         index: indexCount + task.value.length,
         length: task.value.length,
         source: workingText,
-        delay: 50,
+        delay,
       });
     } else {
       indexCount += task.value.length;
     }
   }
-  await eraser({ source: targetText, index: targetText.length });
-  textDiffStart()
+  // 3. 清空文本
+  await eraser({
+    source: targetText,
+    index: targetText.length,
+    length: targetText.length,
+    delay: eraserDelay || delay,
+  });
+  textDiffChange(arguments);
 }
-textDiffStart()
+textDiffChange({ delay: 100, eraserDelay: 1, typewriterDelay: 1 });
