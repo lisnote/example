d2ad03b15cb04e42ccbaba0251843e952d28531e
lisnote <didongxiaoli@gmail.com>
2023-08-31 14:34:21 +0800

    格式化

--- JavaScript/divide-chunk/vite.config.js
+++ JavaScript/divide-chunk/vite.config.js
@@ -4,11 +4,11 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-build: {
-  rollupOptions: {
-    manualChunks: (id) => {
-      return id.split("/").pop();
+  build: {
+    rollupOptions: {
+      manualChunks: (id) => {
+        return id.split("/").pop();
+      },
     },
   },
-},
 });
